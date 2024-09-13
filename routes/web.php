e98a85b91238c60d24d40ce226a842c4f8323d86
shaiko-s <145833407+shaiko-s@users.noreply.github.com>
2024-09-13 17:18:25 +0200

    6 Day codding part 1

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'greeting' => 'Yo',
+        'name' => 'Larry Robot',
+    ]);
 });
 
 Route::get('/about', function () {
