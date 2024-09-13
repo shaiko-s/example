6e0b56597ae263fff0563620f3788a63156eaf56
shaiko-s <145833407+shaiko-s@users.noreply.github.com>
2024-09-13 17:02:57 +0200

    5 Day coding and homework

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,3 +1,13 @@
-{{-- <a href="{{ ($slot != 'Home') ? '/' . strtolower($slot) : '/' }}">{{ $slot }}</a> --}}
+@props(['active' => false,
+        'type' => 'a'])
 
-<a {{ $attributes }}">{{ $slot }}</a>
+@if ($type = 'a')
+    <a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+    aria-current="{{ $active ? 'page' : 'false' }}"
+    {{ $attributes }}
+    >{{ $slot }}</a>
+@else @if ($type = 'button')
+    {{ 'button' }}
+@else
+    {{ 'text' }}
+@endif
