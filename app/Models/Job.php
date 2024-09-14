bc5d2b16dfd57c6fa174cdfcc3f3e07be14dc49a
shaiko-s <145833407+shaiko-s@users.noreply.github.com>
2024-09-14 13:24:29 +0200

    11 Day coding

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model
 {
@@ -12,4 +13,9 @@ class Job extends Model
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
