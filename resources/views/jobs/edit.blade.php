5fc16db09166af3826072c628233fcdfcd3b40aa
shaiko-s <145833407+shaiko-s@users.noreply.github.com>
2024-09-15 17:08:13 +0200

    19 Day cleaning code

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -50,17 +50,6 @@
           </div>
         </div>
 
-        {{-- Validation errors display --}}
-        {{-- <div class="mt-10">
-            @if($errors->any())
-                <ul>
-                    @foreach ($errors->all() as $error )
-                        <li class="text-red-500 italic">{{ $error }}</li>
-                    @endforeach
-                </ul>
-            @endif
-        </div> --}}
-
       </div>
     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -3,26 +3,7 @@
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('home');
-// });
-// =
 Route::view('/', 'home');
-
-// Route::get('/contact', function () {
-//     return view('contact');
-// });
-// =
 Route::view('/contact', 'contact');
 
-// Route::controller(JobController::class)->group(function() {
-//     Route::get('/jobs', 'index');
-//     Route::get('/jobs/create', 'create');
-//     Route::get('/jobs/{job}', 'show');
-//     Route::post('/jobs', 'store');
-//     Route::get('/jobs/{job}/edit', 'edit');
-//     Route::patch('/jobs/{job}', 'update');
-//     Route::delete('/jobs/{job}', 'destroy');
-// });
-// =
 Route::resource('jobs', JobController::class);
