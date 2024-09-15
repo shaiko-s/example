829cc7fb2867cecd5a5a4cb02069cd8c9566f9a7
shaiko-s <145833407+shaiko-s@users.noreply.github.com>
2024-09-15 16:31:02 +0200

    18 Day coding

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function (){
         // $jobs = Job::all(); - lazy loading N+1 problem
     // $jobs = Job::with('employer')->paginate(3);
@@ -17,10 +18,12 @@ Route::get('/jobs', function (){
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function (){
     return view('jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id){
 
     $job = Job::find($id);
@@ -28,6 +31,7 @@ Route::get('/jobs/{id}', function ($id){
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function() {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -42,6 +46,56 @@ Route::post('/jobs', function() {
     ]);
 
     return redirect('jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id){
+
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id){
+    // validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+    // authorize(On hold...)
+
+    // update the job
+    $job = Job::findOrFail($id);
+
+    // $job->title = request('title');
+    // $job->salary = request('salary');
+    // $job->save();
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    // and persist
+    // redirect to the job page
+
+    return redirect('/jobs/' . $job->id);
+
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id){
+    // authorize (On hold... )
+
+    // delete the job
+    $job = Job::findOrFail($id);
+
+    $job->delete();
+
+    //redirect
+    return redirect('/jobs');
+
 });
 
 Route::get('/contact', function () {
